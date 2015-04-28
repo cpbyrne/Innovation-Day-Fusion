3a8610adc20564ff6465e4d4c8f83d696d5316f9
cpbyrne <cpbyrne@yahoo.com>
2015-04-28 19:36:24 -0400

    Got the build up and running!

--- Global.asax.cs
+++ Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,7 +15,9 @@ namespace EmployeeLocationBasedServices
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //GlobalConfiguration.Configure(WebApiConfig.Register);
+            //http://forums.asp.net/t/1807570.aspx
+            //GlobalConfiguration.Configuration.Services.Add(typeof(ModelBinderProvider), new MyModelBinder());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
